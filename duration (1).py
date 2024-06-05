e3aa60c99fec16f0235c896fe032081f533308e4
prajaktaa2000 <bhomleprajakta@gmail.com>
2024-06-05 19:52:14 +0530

    Add files via upload
    uoload

---
+++ duration (1).py
@@ -0,0 +1,29 @@
+def duration_of_survival():
+    age = int(input("What's your age? "))
+    unit = input(
+        "Please choose time unit: Months, Weeks, Days, Hours, Minutes, Seconds. \nNote: You can write the first letter or the full name of the time unit: ").strip().lower()
+
+    months = age * 12
+    weeks = months * 4
+    days = age * 365
+    hours = days * 24
+    minutes = hours * 60
+    seconds = minutes * 60
+
+    if unit == 'months' or unit == 'm':
+        print(f'You lived for {months:,} Months')
+    elif unit == 'weeks' or unit == 'w':
+        print(f'You lived for {weeks:,} Weeks')
+    elif unit == 'days' or unit == 'd':
+        print(f'You lived for {days:,} Days')
+    elif unit == 'hours' or unit == 'h':
+        print(f'You lived for {hours:,} Hours')
+    elif unit == 'minutes' or unit == 'mi':
+        print(f'You lived for {minutes:,} Minutes')
+    elif unit == 'seconds' or unit == 's':
+        print(f'You lived for {seconds:,} Seconds')
+    else:
+        print('Your choice is not available')
+    return
+
+duration_of_survival()
